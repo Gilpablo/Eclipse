56dbd885abd5c8ef3ac86d87dc66b0a68c41955d
Gil Pablo <gilpablo2000.gpbp@gmail.com>
2020-11-17 14:11:04 +0100

    actualización 1.2

--- Ejercicios 1.2. Bucles/src/Ejercicio3.java
+++ Ejercicios 1.2. Bucles/src/Ejercicio3.java
@@ -1,14 +1,14 @@
 
-public class Ejercicio1 {
+public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		int i=1;
-		
+		int i=0;
+				
 		while (i<=100) {
-			
+					
 			System.out.println(i);
-			
-			i++;
+					
+			i=i+2;
 		}
 
 	}
--- Ejercicios 1.2. Bucles/src/Ejercicio4.java
+++ Ejercicios 1.2. Bucles/src/Ejercicio4.java
@@ -1,16 +1,16 @@
 
-public class Ejercicio1 {
+public class Ejercicio4 {
 
 	public static void main(String[] args) {
-		int i=1;
+		int i=1, suma;
 		
 		while (i<=100) {
 			
 			System.out.println(i);
 			
 			i++;
+			//SIN ACABAR
 		}
-
 	}
 
 }
